69f221345c8e5aa1533bd9c64da2bedd883e2854
Cory Spitzer <3945059+CorySpitzer@users.noreply.github.com>
2024-12-30 14:32:56 -0500

    Change the result text

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
 function operate(operator, operand1, operand2) {
+    let result = 0;
     if (operator == '+') {
-        return operand1 + operand2;
+        result = operand1 + operand2;
     } else if (operator == '-') {
-        return operand1 - operand2;
+        result = operand1 - operand2;
     } else if (operator == '*') {
-        return operand1 * operand2;
+        result = operand1 * operand2;
     } else if (operator == '/') {
         if (operand2 === 0) {
-            return "Error: Division by zero";
+            result = "Error: Division by zero";
         }
-        return operand1 / operand2;
+        result = operand1 / operand2;
     } else {
-        return "Error: Invalid operator";
+        result = "Error: Invalid operator";
     }
+    document.getElementById('result').textContent = result
 }
+
+function calculate() {
+    let result  = 'test';
+    document.getElementById('result').textContent = result;
+
+}
+
+document.getElementById('submit-btn').onclick = () => calculate();
+
+// let selectedOperator = 'None selected';
+// document.getElementById('plus').onclick = () => {
+//     selectedOperator = document.querySelector('input[name="operation"]:checked').value;
+//     console.log('Selected operator is ' + selectedOperator);
+// };
+
+// console.log('selectedOperator is ' + selectedOperator);
+
+// let operandA = document.getElementById('firstOperand');
+// let operandB = document.getElementById('secondOperand');
+
+// document.getElementById("submit-btn").onclick = operate(selectedOperator, operandA, operandB);
+
+// document.getElementById('result').textContent = result
