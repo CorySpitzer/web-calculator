172eec43aecbd16664d2a311272e3cef77fbdff5
Cory Spitzer <3945059+CorySpitzer@users.noreply.github.com>
2024-12-30 15:39:52 -0500

    Prevent the form from refreshing the page

--- main.js
+++ main.js
@@ -16,7 +16,6 @@ function operate(operator, operand1, operand2) {
 }
 
 function calculate() {
-    // let operator = document.querySelector('input[name="operation"]:checked').value;
     let operator = 'no operator';
     if (document.getElementById('plus').checked) {
         operator = '+';
@@ -29,26 +28,12 @@ function calculate() {
     }   
     console.log('operator is ' + operator);
     let operand1 = document.getElementById('firstOperand').value;
-    // console.log('1st op is ' + operand1);
     let operand2 = document.getElementById('secondOperand').value;
     let result  = operate(operator, Number(operand1), Number(operand2));
     document.getElementById('result').textContent = result;
-
 }
 
-document.getElementById('submit-btn').onclick = () => calculate();
-
-// let selectedOperator = 'None selected';
-// document.getElementById('plus').onclick = () => {
-//     selectedOperator = document.querySelector('input[name="operation"]:checked').value;
-//     console.log('Selected operator is ' + selectedOperator);
-// };
-
-// console.log('selectedOperator is ' + selectedOperator);
-
-// let operandA = document.getElementById('firstOperand');
-// let operandB = document.getElementById('secondOperand');
-
-// document.getElementById("submit-btn").onclick = operate(selectedOperator, operandA, operandB);
-
-// document.getElementById('result').textContent = result
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    calculate();
+});
